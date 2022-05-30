290cea6e6563a177f8beceb8937c41bc4c77cfd9
Siddharth Prashant Kanikdale <106460051+Siddharth-0381@users.noreply.github.com>
2022-05-30 23:30:28 +0530

    Create MainActivity.java

---
+++ Task_1/Java/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.android.unitconvertor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Converter.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+
+    }
+}
